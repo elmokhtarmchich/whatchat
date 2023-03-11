c6bf78ed87918d23188be1656e936a4f40759a98
elmokhtarmchich <79937072+elmokhtarmchich@users.noreply.github.com>
2023-03-11 20:36:25 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/pwa-examples/a2hs/sw.js')
+    .register('/sw.js')
     .then(() => { console.log('Service Worker Registered'); });
 }
 
