a77d16d269c2dacf7a7c5caee51b12b7ab5524d3
elmokhtarmchich <79937072+elmokhtarmchich@users.noreply.github.com>
2023-08-24 07:40:32 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,22 @@
-var cacheName = 'v1.0';
-
-var cacheAssets = [
-      '/index.html',
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('fox-store').then((cache) => cache.addAll([
+       '/index.html',
       '/index.js',
       '/styles.css',
       '/images/WhatsApp.png',
       '/images/logohead.png',
       '/images/icons-192.png',
       '/images/icons-512.png',
-      '/fox-icon.png',
-]
+      '/icon/app-icon_192x192.png',
+    ])),
+  );
+});
 
-// installation
-self.addEventListener('install', e => {
-      console.log('Service Worker: Installed');
-      e.waitUntil(
-            caches
-              .open(cacheName)
-              .then(cache => {
-                console.log('Service Worker: Caching Files');
-                cache.addAll(cacheAssets);
-              })
-              .then(() => self.skipWaiting())
-          );
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
 
-});    
-
-// activation
-self.addEventListener('activate', e => {
-    console.log('Service Worker: Installed');
-}); 
