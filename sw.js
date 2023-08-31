c5e3c48ff3480c9081b26d34edffef457971c7ae
elmokhtarmchich <79937072+elmokhtarmchich@users.noreply.github.com>
2023-08-31 00:17:51 -0400

    cache edit

--- sw.js
+++ sw.js
@@ -1,11 +1,21 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('app-store').then((cache) => cache.addAll([
-      '/images/WhatsApp.png',
-      '/images/logohead.png',
-      '/images/icons-192.png',
-      '/images/icons-512.png',
-      '/icon/app-icon_192x192.png',
+      '/index.html',
+      '/index.js',
+      '/style.css',
+      '/topnav.css',
+      '/intlTelInput.css',
+      '/manifest.webmanifest',
+      '/sw.js',
+      '/script.js',
+      './icon/app.png',
+    './icon/bell.png',
+    './icon/contact.png',
+    './icon/flags.png',
+    './icon/flags@2x.png',
+    './icon/home.png',
+    './icon/app-icon_192x192.png',
     ])),
   );
 });
