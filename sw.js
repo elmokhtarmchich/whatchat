68c58740a90d02647419c0f12cdcdaf5870bdc0c
elmokhtarmchich <79937072+elmokhtarmchich@users.noreply.github.com>
2023-08-31 00:27:18 -0400

    cache edit

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('app-store').then((cache) => cache.addAll([
+    caches.open('watsapgo-store').then((cache) => cache.addAll([
       '/index.html',
       '/index.js',
       '/style.css',
