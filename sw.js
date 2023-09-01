4a0095e7aa3d191eaf39e549165389df51b44686
elmokhtarmchich <79937072+elmokhtarmchich@users.noreply.github.com>
2023-09-01 02:32:30 -0400

    sw

--- sw.js
+++ sw.js
@@ -3,7 +3,14 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('app-store').then((cache) => cache.addAll([
-      '/index.html',
+      './',
+      'index.html',
+      'style.css',
+      'intlTelInput.css',
+      'topnav.css',
+      'icon/app-icon_192x192.png',
+      'icon/bell.png',
+      'icon/contact.png',
     ])),
   );
 });
